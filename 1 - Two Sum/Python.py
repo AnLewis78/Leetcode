7497518fd6e4732725a6b6559058aee5c0b3ee06
Andrew Lewis <anlewis78@gmail.com>
2023-01-28 16:41:18 -0600

    Slow method that works, room for improvement

--- 1 - Two Sum/Python.py
+++ 1 - Two Sum/Python.py
@@ -4,10 +4,26 @@ def twoSum(nums, target):
         :type target: int
         :rtype: List[int]
         """
-        pass
+        p0 = 0; p1 = len(nums) -1
 
+        while True:
+            sum = nums[p0] + nums[p1]
+
+            if p0 == p1:
+                #counter += 1
+                p1 = len(nums) -1 
+                p0 += 1
+            elif sum == target:
+                return [p0, p1]
+            else:
+                p1 -= 1
+
+            
+# Public Case 1
 print(twoSum([2,7,11,15], 9))
 
-print(twoSum([3,2,4]), 6)
+# Public Case 2
+print(twoSum([3,2,4], 6))
 
-print(twoSum([3,3]), 6)
+# Public Case 3
+print(twoSum([3,3], 6))
