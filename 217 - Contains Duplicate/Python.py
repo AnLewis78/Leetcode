4793379efbd3658ee756f2ecb9453e6cc4d75d4a
Andrew Lewis <anlewis78@gmail.com>
2023-01-28 13:18:00 -0600

    Initalized Problem

---
+++ 217 - Contains Duplicate/Python.py
@@ -0,0 +1,15 @@
+def containsDuplicate(nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        pass
+
+# Case 1
+print(containsDuplicate([1,2,3,1]))
+
+# Case 2
+print(containsDuplicate([1,2,3,4]))
+
+# Case 3
+print(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
