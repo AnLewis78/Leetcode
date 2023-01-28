e281f6d6f894dd3edcd7135f2b2ef92db21fec5b
Andrew Lewis <anlewis78@gmail.com>
2023-01-28 15:50:00 -0600

    initialization of Problem 1

---
+++ 1 - Two Sum/Python.py
@@ -0,0 +1,13 @@
+def twoSum(nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        pass
+
+print(twoSum([2,7,11,15], 9))
+
+print(twoSum([3,2,4]), 6)
+
+print(twoSum([3,3]), 6)
