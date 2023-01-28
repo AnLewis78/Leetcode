62d28825fbf936f7f97799d26a19a4f731a5105b
Andrew Lewis <anlewis78@gmail.com>
2023-01-28 13:53:37 -0600

    Approach using dictionary. Passed all tests

--- 217 - Contains Duplicate/Python.py
+++ 217 - Contains Duplicate/Python.py
@@ -1,19 +1,26 @@
+"""
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+"""
+
 def containsDuplicate(nums):
         """
         :type nums: List[int]
         :rtype: bool
         """
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] == nums[j]:
-                    return True
+        num_dict = {}
+        for num in nums:
+            if num in num_dict:
+                return True
+            else:
+                num_dict[num] = 1
         return False
 
-# Case 1
+# Public Case 1
 print(containsDuplicate([1,2,3,1]))
 
-# Case 2
+# Public Case 2
 print(containsDuplicate([1,2,3,4]))
 
-# Case 3
+# Public Case 3
 print(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
