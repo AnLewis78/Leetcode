a0b13bfd0c3fb2e994f097c79186609df67db454
Andrew Lewis <anlewis78@gmail.com>
2023-01-28 14:20:22 -0600

    Initalize Problem 242

---
+++ 242 - Valid Anagram/Python.py
@@ -0,0 +1,13 @@
+def isAnagram(s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        pass
+
+# Public Case 1
+print(isAnagram("anagram", "nagaram"))
+
+# Public Case 2
+print(isAnagram("rat", "car"))
