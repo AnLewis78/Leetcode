7eb06d0c1297551f01a9d16ef32b6517fd43028a
Andrew Lewis <anlewis78@gmail.com>
2023-01-28 23:00:52 -0600

    Initalization of Problem 49

---
+++ 49 - Group Anagrams/Python.py
@@ -0,0 +1,14 @@
+def groupAnagrams(strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+
+# Public Case 1
+print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+
+# Public Case 2
+print(groupAnagrams([""]))
+
+#public Case 3
+print(groupAnagrams(["a"]))
