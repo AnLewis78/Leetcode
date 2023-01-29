1355d791e4cd54749bce48e5d3f73d786e3a7669
Andrew Lewis <anlewis78@gmail.com>
2023-01-29 13:44:26 -0600

    Initalized Problem 347

---
+++ 347 - Top K Frequent Elements/Python.py
@@ -0,0 +1,12 @@
+def topKFrequent(nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+
+# Public Case One
+print(topKFrequent([1,1,1,2,2,3], 2))
+
+# Public Case Two
+print(topKFrequent([1], 1))
