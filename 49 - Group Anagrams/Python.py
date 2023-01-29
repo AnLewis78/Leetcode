83cc44a8cdb320f31546ba8383d44fc70c6f8ef3
Andrew Lewis <anlewis78@gmail.com>
2023-01-29 00:03:41 -0600

    Working Solution. Need to Practice this more

--- 49 - Group Anagrams/Python.py
+++ 49 - Group Anagrams/Python.py
@@ -3,6 +3,13 @@ def groupAnagrams(strs):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
+        ans = {}
+        for s in strs:
+            sorted_s = ''.join(sorted(s))
+            if sorted_s not in ans:
+                ans[sorted_s] = []
+            ans[sorted_s].append(s)
+        return list(ans.values())
 
 # Public Case 1
 print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
