60a349b657e2798045922d6b4ffa8ccc2785aea9
Andrew Lewis <anlewis78@gmail.com>
2023-01-29 19:34:54 -0600

    Initalized Problem

---
+++ 238 - Product of Array Except Self/Python.py
@@ -0,0 +1,11 @@
+def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+
+# Public Case 1
+print(productExceptSelf([1,2,3,4]))
+
+# Public Case 2
+print(productExceptSelf([-1,1,0,-3,3]))
