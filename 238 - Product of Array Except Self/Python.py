1cf9e12006612901ddb318aa4e3b4eab7d34d921
Andrew Lewis <anlewis78@gmail.com>
2023-01-29 19:50:07 -0600

    Working solution. Time limit exception error

--- 238 - Product of Array Except Self/Python.py
+++ 238 - Product of Array Except Self/Python.py
@@ -1,8 +1,21 @@
-def productExceptSelf(self, nums):
+def productExceptSelf(nums):
         """
         :type nums: List[int]
         :rtype: List[int]
         """
+        answer = []
+        temp = 1
+        # Go through each of the element in the list
+        for i in range(len(nums)):
+            for count, value in enumerate(nums):
+                if count != i:
+                    temp *= value
+            answer.append(temp)
+            temp = 1
+        return answer
+        
+
+
 
 # Public Case 1
 print(productExceptSelf([1,2,3,4]))
